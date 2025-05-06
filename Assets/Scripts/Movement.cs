8f3e435d65ee6eeeaab65081d52af9a58c99f365
Sealsealgitproj <Jirakit.somb@bumail.net>
2025-05-06 08:50:39 +0700

    Movementupdate
    Added movement to the character

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    float move;
+
+    public float jumpforce;
+    
+    Rigidbody2D rb2d;
+    
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        move = Input.GetAxis("Horizontal");
+        rb2d.linearVelocity = new Vector2(move * speed, rb2d.linearVelocity.y);
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2d.AddForce(new Vector2(rb2d.linearVelocity.x, jumpforce));
+            Debug.Log("Jump"); 
+    }
+}
+    
+}
+
